fa5c9c35554ce79a61df2be3fb4958ce2259a833
Chan Jing Hong <jinghongchan@gmail.com>
2021-06-14 13:56:18 +0800

    init

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>
+          💰💰
+        </h1>
+        <a
+          className="App-link"
+          href="https://twitter.com/cjinghong"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          @cjinghong
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
